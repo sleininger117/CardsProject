8abc28c96f44377dceea0480ead5c7de01501ee0
Brian A Burnett <bbrianmiraleste@gmail.com>
2023-06-05 16:42:48 -0700

    Contacts Functionality
    Add "Industry" folder when demoing

--- app.js
+++ app.js
@@ -40,4 +40,38 @@ app.get('/notifications', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notification.html'));
 })
 
+app.get('/folders', (req, res) => {
+    res.statusCode = 200;
+    res.sendFile(path.join(__dirname, '/public/folders.html'));
+})
+
+app.get('/folders2', (req, res) => {
+    res.statusCode = 200;
+    res.sendFile(path.join(__dirname, '/public/folders2.html'));
+})
+
+app.get('/folders3', (req, res) => {
+    res.statusCode = 200;
+    res.sendFile(path.join(__dirname, '/public/folders3.html'));
+})
+
+app.get('/folders4', (req, res) => {
+    res.statusCode = 200;
+    res.sendFile(path.join(__dirname, '/public/folders4.html'));
+})
+
+app.get('/folders5', (req, res) => {
+    res.statusCode = 200;
+    res.sendFile(path.join(__dirname, '/public/folders5.html'));
+})
+
+app.get('/folders6', (req, res) => {
+    res.statusCode = 200;
+    res.sendFile(path.join(__dirname, '/public/folders6.html'));
+})
+app.get('/addFolder', (req, res) => {
+    res.statusCode = 200;
+    res.sendFile(path.join(__dirname, '/public/addFolder.html'));
+})
+
 module.exports = app;
